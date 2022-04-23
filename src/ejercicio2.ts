8e28e306a47aca8d5f3a1456c33cd79cdc932f60
lldini <104235015+lldini@users.noreply.github.com>
2022-04-22 22:25:51 -0300

    primero

---
+++ src/ejercicio2.ts
@@ -0,0 +1,6 @@
+let numero1: number = Number(prompt("ingrese numero 1"));
+let numero2: number = Number(prompt("ingrese numero 2"));
+
+let resultado: number = numero1 + numero2;
+
+console.log("el resultado es: ", resultado);
---
+++ src/ejercicio3.ts
@@ -0,0 +1,6 @@
+//el programa imprime 4 lineas
+console.log("este");
+console.log("es");
+console.log("un");
+console.log("algoritmo");
+console.log("secuencial");
