c8dbcfc1ff3d756cf4499510f2461a255e5dcdb5
lldini <104235015+lldini@users.noreply.github.com>
2022-04-29 19:49:06 -0300

    comente el prompt para que no moleste

--- src/ejercicio2.ts
+++ src/ejercicio2.ts
@@ -1,5 +1,5 @@
-let numero1: number = Number(prompt("ingrese numero 1"));
-let numero2: number = Number(prompt("ingrese numero 2"));
+// let numero1: number = Number(prompt("ingrese numero 1"));
+// let numero2: number = Number(prompt("ingrese numero 2"));
 
 let resultado: number = numero1 + numero2;
 
